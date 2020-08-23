d8cb0f4b41917f0be90ef349fc5737c4382e09ca
SendyMederos <sendymederos@hotmail.com>
2020-08-23 17:33:46 -0400

    fixing somethings

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ $.ajax({
     var lat = response.coord.lat
     var lon = response.coord.lon
     var iconCode = response.weather[0].icon;
-    var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+    var iconUrl = "https://openweathermap.org/img/w/" + iconCode + ".png";
     $("#currentWeather").append(`<h2>${response.name}</h2>`)
     
     var oneCallURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&" + "lon=" + lon + "&units=imperial&exclude=minutely,hourly&appid=420fa54141903a76b9ac423622e9920d"
@@ -21,16 +21,16 @@ $.ajax({
         console.log(response)
         $("#currentWeather").append('<h3>' + time + '</h3>')
         $("#currentWeather").append(`<p>${response.current.temp}</p>`)
-        $("#currentWeather").append(`<img src = "${iconUrl}">`)
+        $("#currentWeather").append(`<img src = "${iconUrl}"class="icon">`)
         $("#currentWeather").append(`<p>${response.current.uvi}</p>`)
 
         var fivedays = ["dayOne", "dayTwo", "dayThree", "dayFourth", "dayFive"]
         for (i = 1; i < 6; i++) {
             iconCode = response.daily[i].weather[0].icon;
-            iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+            iconUrl = "https://openweathermap.org/img/w/" + iconCode + ".png";
             $("#five-days").append(`<div id = "${fivedays[i]}" class = "five-days card" > </div>`);
             $(`#${fivedays[i]}`).append(`<h5>${moment(response.daily[i].dt * 1000).format("dddd,  MMMM DD")}</h5>`);
-            $(`#${fivedays[i]}`).append(`<img src = "${iconUrl}" class="text-center">`);
+            $(`#${fivedays[i]}`).append(`<img src = "${iconUrl}" class="icon">`);
             $(`#${fivedays[i]}`).append(`<p>Temp: ${response.daily[i].temp.day}°F</p>`);
             $(`#${fivedays[i]}`).append(`<p>Humidity: ${response.daily[i].humidity}%</p>`);
         }
